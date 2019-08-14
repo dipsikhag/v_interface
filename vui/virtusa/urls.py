a3bc561379d41c85f2a7cffc1e5ca2d3081480ad
Dipsikha Das Gupta <dipsikhadg@virtusa.com>
2019-08-14 17:44:59 +0530

    'poc_changes'

--- vui/virtusa/urls.py
+++ vui/virtusa/urls.py
@@ -3,6 +3,6 @@ from django.urls import path,include
 import virtusa.views as vview
 
 urlpatterns = [
-    path('/', vview.modelsview),
+    path('', vview.modelsview),
     # path('<int:item_url>',vview.modelsresult,name='modelsresult'),
 ]
--- vui/virtusa/views.py
+++ vui/virtusa/views.py
@@ -5,7 +5,12 @@ from virtusa import models
 def modelsview(request):
     k = models.Ml_models.objects.all()
     mlist = [{'name':i.name,'image':i.image,'summary':i.summary } for i in k]
-    return render(request,'models.html',{'models':mlist})
-# 
+    return render(request,'index.html',{'models':mlist})
+#
 # def modelsresult(request,item_url):
 #     return render(request,'models.html',{'models':mlist})
+def indmodel(request):
+    name = request.GET['model']
+    k = models.Ml_models.get(name=name)
+    mlist = [{'name':i.name,'image':i.image,'summary':i.summary } for i in k]
+    return render(request,'models.html',{'models':mlist})
